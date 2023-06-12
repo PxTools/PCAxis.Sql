0c319d01cdb0c12c6224ea0208c2e0e9a2758460
petroslikidis <petros.likidis@gmail.com>
2023-06-12 18:38:04 +0200

    Added repos for valuesets and aggregations

---
+++ PCAxis.Sql/Models/Value.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PCAxis.Sql.Models
+{
+    public class Value
+    {
+        public string Code { get; set; }
+        public string Text { get; set; }
+
+        public Value()
+        {
+
+        }
+    }
+}
---
+++ PCAxis.Sql/QueryLib_24/Queries.cs
@@ -0,0 +1,101 @@
+﻿using PCAxis.Sql.DbClient;
+using PCAxis.Sql;
+using PCAxis.Sql.DbConfig;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCAxis.Sql.QueryLib_24
+{
+    public class Queries
+    {
+
+        public static string GetValueSetQuery(SqlDbConfig_24 db, string lang)
+        {
+            if (db == null) throw new ArgumentNullException("db");
+
+            return $@"select 
+                         {db.ValueSetLang2.ValueSetCol.Id(lang)} AS ValueSet,
+                         {db.ValueSetLang2.PresTextCol.Id(lang)} AS PresText,
+                         {db.ValueSetLang2.DescriptionCol.Id(lang)} AS Description
+                        from 
+                         {db.ValueSetLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()}
+                        where
+                         {db.ValueSetLang2.ValueSetCol.Id(lang)} = @valueSet";
+        }
+
+        public static string GetValueSetValuesQuery(SqlDbConfig_24 db, string lang)
+        {
+            if (db == null) throw new ArgumentNullException("db");
+
+                return $@"SELECT
+                            {db.ValueLang2.ValueCodeCol.Id(lang)} AS valuecode,
+	                        {db.ValueLang2.ValuePoolCol.Id(lang)}  AS valuepool,
+	                        {db.VSValueLang2.ValueCodeCol.Id(lang)}  AS valueset,
+                            {db.ValueLang2.ValueTextLCol.Id(lang)} AS valuetextl,
+                            {db.ValueLang2.ValueTextSCol.Id(lang)} AS valuetexts,
+                            {db.ValueLang2.SortCodeCol.Id(lang)}   AS sortcodevalue,
+	                        {db.VSValueLang2.ValueCodeCol.Id(lang)} AS sortcodevsvalue
+                        FROM
+                            {db.ValueLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()}
+                        JOIN 
+                            {db.VSValueLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()}
+	                        ON ( {db.ValueLang2.ValuePoolCol.Id(lang)} = {db.VSValueLang2.ValuePoolCol.Id(lang)}
+                                AND {db.ValueLang2.ValueCodeCol.Id(lang)} = {db.VSValueLang2.ValueCodeCol.Id(lang)} ) 
+                        where {db.VSValueLang2.ValueSetCol.Id(lang)} = @valueSet
+
+                        ORDER BY
+                            {db.VSValueLang2.SortCodeCol.Id(lang)},
+                            {db.ValueLang2.SortCodeCol.Id(lang)},
+                            {db.ValueLang2.ValueCodeCol.Id(lang)}";
+
+        }
+        public static string GetGroupingQuery(SqlDbConfig_24 db, string lang)
+        {
+            if (db == null) throw new ArgumentNullException("db");
+
+            return $@"SELECT
+	                        {db.GroupingLang2.GroupingCol.Id(lang)} AS Grouping, 
+	                        {db.GroupingLang2.PresTextCol.Id(lang)} AS Name
+                        FROM 
+	                        {db.GroupingLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()}
+                        WHERE
+	                        {db.GroupingLang2.GroupingCol.Id(lang)} = @grouping";
+        }
+
+        public static string GetGroupingValuesQuery(SqlDbConfig_24 db, string lang)
+        {
+            if (db == null) throw new ArgumentNullException("db");
+
+            return $@"select 
+                         {db.ValueGroup.GroupCodeCol.Id()} AS GroupCode,
+                         {db.ValueGroup.ValueCodeCol.Id()} AS ValueCode,
+                         {db.ValueLang2.ValueTextLCol.Id(lang)} AS TEXTL,
+                         {db.ValueLang2.ValueTextSCol.Id(lang)} AS TEXTS
+                        from {db.ValueGroup.GetNameAndAlias().RemoveUnderscoreForDefaultLanguage()}
+                         join {db.ValueLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()} 
+                                on {db.ValueGroup.ValuePoolCol.Id()} = {db.ValueLang2.ValuePoolCol.Id(lang)} and {db.ValueGroup.GroupCodeCol.Id()} = {db.ValueLang2.ValueCodeCol.Id(lang)}
+                        where {db.ValueGroup.GroupingCol.Id()} = @grouping
+                        ORDER BY
+                        {db.ValueGroup.SortCodeCol.Id()},
+                        {db.ValueLang2.SortCodeCol.Id(lang)},
+                        {db.ValueGroup.GroupCodeCol.Id()}";
+
+        }
+    }
+
+
+    public static class TableLangFixer
+    {
+        public static string RemoveUnderscoreForDefaultLanguage(this string name)
+        {
+            if (name.Contains("_ "))
+            {
+                return name.Replace("_ "," ");
+            }
+            return name;
+        }
+    }
+}
