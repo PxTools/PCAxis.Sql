1749420a4f77f207941b32b9813f750013160fda
Johannes <jfi@ssb.no>
2024-12-20 10:40:40 +0100

    Introducing AbstractQueries class

--- PCAxis.Sql/QueryLib_23/Queries.cs
+++ PCAxis.Sql/QueryLib_23/Queries.cs
@@ -1,34 +1,39 @@
 ﻿using System;
 
 using PCAxis.Sql.DbConfig;
+using PCAxis.Sql.Repositories;
 
 namespace PCAxis.Sql.QueryLib_23
 {
-    public class Queries
+    internal class Queries : AbstractQueries
     {
-        public static string GetValueSetExistsIn(SqlDbConfig_23 db, string lang, PxSqlCommand sqlCommand)
+        private readonly SqlDbConfig_23 _db;
+        internal Queries(SqlDbConfig db)
         {
             if (db == null) throw new ArgumentNullException("db");
 
-            return $@"select '{lang}' As Language
-                        from 
-                         {db.ValueSetLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()}
-                        where
-                         {db.ValueSetLang2.ValueSetCol.Id(lang)} = {sqlCommand.GetParameterRef("aValueSet")}";
+            _db = (SqlDbConfig_23)db;
         }
 
-        public static string GetValueSetQuery(SqlDbConfig_23 db, string lang, PxSqlCommand sqlCommand)
+        internal override string GetValueSetExistsIn(string lang, PxSqlCommand sqlCommand)
         {
-            if (db == null) throw new ArgumentNullException("db");
-
-            return $@"select 
-                         {db.ValueSetLang2.ValueSetCol.Id(lang)} AS ValueSet,
-                         {db.ValueSetLang2.PresTextCol.Id(lang)} AS PresText,
-                         {db.ValueSetLang2.DescriptionCol.Id(lang)} AS Description
+            return $@"select '{lang}' As Language
                         from 
-                         {db.ValueSetLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()}
+                         {_db.ValueSetLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()}
                         where
-                         {db.ValueSetLang2.ValueSetCol.Id(lang)} = {sqlCommand.GetParameterRef("aValueSet")}";
+                         {_db.ValueSetLang2.ValueSetCol.Id(lang)} = {sqlCommand.GetParameterRef("aValueSet")}";
+        }
+
+        internal override string GetValueSetQuery(string lang, PxSqlCommand sqlCommand)
+        {
+            return $@"select 
+                         {_db.ValueSetLang2.ValueSetCol.Id(lang)} AS ValueSet,
+                         {_db.ValueSetLang2.PresTextCol.Id(lang)} AS PresText,
+                         {_db.ValueSetLang2.DescriptionCol.Id(lang)} AS Description
+                        from 
+                         {_db.ValueSetLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()}
+                        where
+                         {_db.ValueSetLang2.ValueSetCol.Id(lang)} = {sqlCommand.GetParameterRef("aValueSet")}";
 
             //if (db.isSecondaryLanguage(lang))
             //{
@@ -54,83 +59,68 @@ namespace PCAxis.Sql.QueryLib_23
 
         }
 
-        public static string GetValueSetValuesQuery(SqlDbConfig_23 db, string lang, PxSqlCommand sqlCommand)
+        internal override string GetValueSetValuesQuery(string lang, PxSqlCommand sqlCommand)
         {
-            if (db == null) throw new ArgumentNullException("db");
-
             return $@"SELECT
-                            {db.ValueLang2.ValueCodeCol.Id(lang)} AS valuecode,
-	                        {db.ValueLang2.ValuePoolCol.Id(lang)}  AS valuepool,
-	                        {db.VSValueLang2.ValueCodeCol.Id(lang)}  AS valueset,
-                            {db.ValueLang2.ValueTextLCol.Id(lang)} AS valuetextl,
-                            {db.ValueLang2.ValueTextSCol.Id(lang)} AS valuetexts,
-                            {db.ValueLang2.SortCodeCol.Id(lang)}   AS sortcodevalue,
-	                        {db.VSValueLang2.ValueCodeCol.Id(lang)} AS sortcodevsvalue
+                            {_db.ValueLang2.ValueCodeCol.Id(lang)} AS valuecode,
+	                        {_db.ValueLang2.ValuePoolCol.Id(lang)}  AS valuepool,
+	                        {_db.VSValueLang2.ValueCodeCol.Id(lang)}  AS valueset,
+                            {_db.ValueLang2.ValueTextLCol.Id(lang)} AS valuetextl,
+                            {_db.ValueLang2.ValueTextSCol.Id(lang)} AS valuetexts,
+                            {_db.ValueLang2.SortCodeCol.Id(lang)}   AS sortcodevalue,
+	                        {_db.VSValueLang2.ValueCodeCol.Id(lang)} AS sortcodevsvalue
                         FROM
-                            {db.ValueLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()}
+                            {_db.ValueLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()}
                         JOIN 
-                            {db.VSValueLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()}
-	                        ON ( {db.ValueLang2.ValuePoolCol.Id(lang)} = {db.VSValueLang2.ValuePoolCol.Id(lang)}
-                                AND {db.ValueLang2.ValueCodeCol.Id(lang)} = {db.VSValueLang2.ValueCodeCol.Id(lang)} ) 
-                        where {db.VSValueLang2.ValueSetCol.Id(lang)} = {sqlCommand.GetParameterRef("aValueSet")}
+                            {_db.VSValueLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()}
+	                        ON ( {_db.ValueLang2.ValuePoolCol.Id(lang)} = {_db.VSValueLang2.ValuePoolCol.Id(lang)}
+                                AND {_db.ValueLang2.ValueCodeCol.Id(lang)} = {_db.VSValueLang2.ValueCodeCol.Id(lang)} ) 
+                        where {_db.VSValueLang2.ValueSetCol.Id(lang)} = {sqlCommand.GetParameterRef("aValueSet")}
 
                         ORDER BY
-                            {db.VSValueLang2.SortCodeCol.Id(lang)},
-                            {db.ValueLang2.SortCodeCol.Id(lang)},
-                            {db.ValueLang2.ValueCodeCol.Id(lang)}";
+                            {_db.VSValueLang2.SortCodeCol.Id(lang)},
+                            {_db.ValueLang2.SortCodeCol.Id(lang)},
+                            {_db.ValueLang2.ValueCodeCol.Id(lang)}";
 
         }
 
-        public static string GetGroupingExistsIn(SqlDbConfig_23 db, string lang, PxSqlCommand sqlCommand)
+        internal override string GetGroupingQuery(string lang, PxSqlCommand sqlCommand)
         {
-            if (db == null) throw new ArgumentNullException("db");
+            return $@"SELECT
+	                        {_db.GroupingLang2.GroupingCol.Id(lang)} AS Grouping, 
+	                        {_db.GroupingLang2.PresTextCol.Id(lang)} AS Name
+                        FROM 
+	                        {_db.GroupingLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()}
+                        WHERE
+	                        {_db.GroupingLang2.GroupingCol.Id(lang)} = {sqlCommand.GetParameterRef("aGrouping")}";
+        }
 
+        internal override string GetGroupingExistsIn(string lang, PxSqlCommand sqlCommand)
+        {
             return $@"select '{lang}' As Language
                         from 
-                          {db.GroupingLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()}
+                          {_db.GroupingLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()}
                         where
-                          {db.GroupingLang2.GroupingCol.Id(lang)} = {sqlCommand.GetParameterRef("aGrouping")}";
-        }
-        public static string GetGroupingQuery(SqlDbConfig_23 db, string lang, PxSqlCommand sqlCommand)
-        {
-            if (db == null) throw new ArgumentNullException("db");
-
-            return $@"SELECT
-	                        {db.GroupingLang2.GroupingCol.Id(lang)} AS Grouping, 
-	                        {db.GroupingLang2.PresTextCol.Id(lang)} AS Name
-                        FROM 
-	                        {db.GroupingLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()}
-                        WHERE
-	                        {db.GroupingLang2.GroupingCol.Id(lang)} = {sqlCommand.GetParameterRef("aGrouping")}";
+                          {_db.GroupingLang2.GroupingCol.Id(lang)} = {sqlCommand.GetParameterRef("aGrouping")}";
         }
 
-        public static string GetGroupingValuesQuery(SqlDbConfig_23 db, string lang, PxSqlCommand sqlCommand)
-        {
-            if (db == null) throw new ArgumentNullException("db");
 
+        internal override string GetGroupingValuesQuery(string lang, PxSqlCommand sqlCommand)
+        {
             return $@"select 
-                         {db.ValueGroup.GroupCodeCol.Id()} AS GroupCode,
-                         {db.ValueGroup.ValueCodeCol.Id()} AS ValueCode,
-                         {db.ValueLang2.ValueTextLCol.Id(lang)} AS TEXTL,
-                         {db.ValueLang2.ValueTextSCol.Id(lang)} AS TEXTS
-                        from {db.ValueGroup.GetNameAndAlias().RemoveUnderscoreForDefaultLanguage()}
-                         join {db.ValueLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()} 
-                                on {db.ValueGroup.ValuePoolCol.Id()} = {db.ValueLang2.ValuePoolCol.Id(lang)} and {db.ValueGroup.GroupCodeCol.Id()} = {db.ValueLang2.ValueCodeCol.Id(lang)}
-                        where {db.ValueGroup.GroupingCol.Id()} = {sqlCommand.GetParameterRef("aGrouping")}
+                         {_db.ValueGroup.GroupCodeCol.Id()} AS GroupCode,
+                         {_db.ValueGroup.ValueCodeCol.Id()} AS ValueCode,
+                         {_db.ValueLang2.ValueTextLCol.Id(lang)} AS TEXTL,
+                         {_db.ValueLang2.ValueTextSCol.Id(lang)} AS TEXTS
+                        from {_db.ValueGroup.GetNameAndAlias().RemoveUnderscoreForDefaultLanguage()}
+                         join {_db.ValueLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()} 
+                                on {_db.ValueGroup.ValuePoolCol.Id()} = {_db.ValueLang2.ValuePoolCol.Id(lang)} and {_db.ValueGroup.GroupCodeCol.Id()} = {_db.ValueLang2.ValueCodeCol.Id(lang)}
+                        where {_db.ValueGroup.GroupingCol.Id()} = {sqlCommand.GetParameterRef("aGrouping")}
                         ORDER BY
-                        {db.ValueGroup.SortCodeCol.Id()},
-                        {db.ValueLang2.SortCodeCol.Id(lang)},
-                        {db.ValueGroup.GroupCodeCol.Id()}";
+                        {_db.ValueGroup.SortCodeCol.Id()},
+                        {_db.ValueLang2.SortCodeCol.Id(lang)},
+                        {_db.ValueGroup.GroupCodeCol.Id()}";
 
-            //return $@"select 
-            //             {db.ValueGroupLang2.GroupCodeCol.Id(lang)} AS GroupCode,
-            //             {db.ValueGroupLang2.ValueCodeCol.Id(lang)} AS ValueCode,
-            //             {db.ValueLang2.ValueTextLCol.Id(lang)} AS TEXTL,
-            //             {db.ValueLang2.ValueTextSCol.Id(lang)} AS TEXTS
-            //            from {db.ValueGroupLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()}
-            //             join {db.ValueLang2.GetNameAndAlias(lang).RemoveUnderscoreForDefaultLanguage()} 
-            //                    on {db.ValueGroupLang2.ValuePoolCol.Id(lang)} = {db.ValueLang2.ValuePoolCol.Id(lang)} and {db.ValueGroupLang2.GroupCodeCol.Id(lang)} = {db.ValueLang2.ValueCodeCol.Id(lang)}
-            //            where {db.ValueGroupLang2.GroupingCol.Id(lang)} = @grouping";
         }
     }
 
---
+++ PCAxis.Sql/Repositories/AbstractQueries.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using PCAxis.Sql.DbConfig;
+
+namespace PCAxis.Sql.Repositories
+{
+    internal abstract class AbstractQueries
+    {
+        internal abstract string GetValueSetExistsIn(string lang, PxSqlCommand sqlCommand);
+
+        internal abstract string GetValueSetQuery(string lang, PxSqlCommand sqlCommand);
+
+        internal abstract string GetValueSetValuesQuery(string lang, PxSqlCommand sqlCommand);
+
+        internal abstract string GetGroupingExistsIn(string lang, PxSqlCommand sqlCommand);
+
+        internal abstract string GetGroupingQuery(string lang, PxSqlCommand sqlCommand);
+
+        internal abstract string GetGroupingValuesQuery(string lang, PxSqlCommand sqlCommand);
+
+        internal static AbstractQueries GetSqlqueries(SqlDbConfig config)
+        {
+            if (config.MetaModel.Equals("2.1"))
+            {
+                return new PCAxis.Sql.QueryLib_21.Queries(config);
+            }
+            else if (config.MetaModel.Equals("2.2"))
+            {
+                return new PCAxis.Sql.QueryLib_22.Queries(config);
+            }
+            else if (config.MetaModel.Equals("2.3"))
+            {
+                return new PCAxis.Sql.QueryLib_23.Queries(config);
+            }
+            else if (config.MetaModel.Equals("2.4"))
+            {
+                return new PCAxis.Sql.QueryLib_24.Queries(config);
+            }
+            else
+            {
+                throw new NotImplementedException("Unknown MetaModel version: " + config.MetaModel);
+            }
+
+
+        }
+    }
+}
