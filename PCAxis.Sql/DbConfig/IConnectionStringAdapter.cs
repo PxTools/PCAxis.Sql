01a54004efdb41451606f68d594c46fe9fac8596
StefanDK <stefangunnersen@gmail.com>
2025-03-24 11:20:28 +0100

    Extends databasse capability to override connectionString dynamically if needed. Is optional and is only in effect if an attribute called connectionStringAdapterType is added to SqlDb.config for the element Database->Connection. The attribute value must be a class that inherits from IConnectionStringAdapter and implements the metod ReplaceConnectionString(string) which effectively overrides the connection string being used for connecting to the CNMM database. This can be effective (and needed) if a switch between databases is needed.

---
+++ PCAxis.Sql/DbConfig/IConnectionStringAdapter.cs
@@ -0,0 +1,16 @@
+﻿namespace PCAxis.Sql.DbConfig
+{
+    /// <summary>
+    /// Makes it possible to overrule the processing of the connection string,
+    /// </summary>
+    public interface IConnectionStringAdapter
+    {
+        /// <summary>
+        /// Replaces the connection string with a different one based on a local implementation.
+        /// If nothing can or will be processed the method should return the unaltered connectionString variable.
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
+        string ReplaceConnectionString(string connectionString);
+    }
+}
