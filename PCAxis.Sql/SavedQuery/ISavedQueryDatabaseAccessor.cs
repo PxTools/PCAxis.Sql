d6d6584a6d0dfdf1bb4dcc1107b5fc04490d00f2
likp <petros.likidis@scb.se>
2025-04-04 15:20:59 +0200

    Just made it possible to compile

---
+++ PCAxis.Sql/SavedQuery/ISavedQueryDatabaseAccessor.cs
@@ -0,0 +1,11 @@
+﻿namespace PCAxis.Sql.SavedQuery
+{
+    public interface ISavedQueryDatabaseAccessor
+    {
+        string Load(int id);
+
+        int Save(string savedQuery, int? id);
+
+        bool MarkAsRunned(int name);
+    }
+}
